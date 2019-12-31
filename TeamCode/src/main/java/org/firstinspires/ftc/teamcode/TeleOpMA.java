e32c3c40cac59a7cd3ffacfcd9da259eb384801f
Sid Young <sidyoung2@gmail.com>
2019-12-30 20:20:16 -0800

    code cleanup.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMA.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMA.java
@@ -5,23 +5,23 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp(name = "TeleOpMA", group = "")
 public class TeleOpMA extends CommonOpMode {
 
-  @Override
-  public void runOpMode() {
+    @Override
+    public void runOpMode() {
 
-    initHardware();
-    armReset();
-    
-    waitForStart();
-    
-    while (opModeIsActive()) {
-      drive();
-      arms();
-      suction();
-      lift();
-      lineUpBlock();
-      strafeAuto();
-      Grabber.setPosition(1);
-      telemetry.update();
+        initHardware();
+        armReset();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            drive();
+            arms();
+            suction();
+            lift();
+            lineUpBlock();
+            strafeAuto();
+            Grabber.setPosition(1);
+            telemetry.update();
+        }
     }
-  }
 }
